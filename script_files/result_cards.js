e34a45f25cea38ed44eb90e768b3e0e4615b3fb0
DestinyJoyner <destinyjoyner@pursuit.org>
2024-01-21 23:08:27 -0500

    able to fetch results and append them to DOM on client side

--- script_files/result_cards.js
+++ script_files/result_cards.js
@@ -2,6 +2,16 @@ import { fetchResults } from "./fetchFunctions.js";
 
 try {
     const resultsObj = await fetchResults();
+    // iterate through keys in obj
+    for(const key in resultsObj){
+        // create a card div
+        const pollCard = document.createElement("div")
+        const pollCardValue = document.createElement("h2")
+        pollCardValue.textContent= `${key}: ${resultsObj[key]}`
+        pollCard.append(pollCardValue)
+        document.getElementById(`pollcard-container`).append(pollCard)
+        
+    }
     console.log(resultsObj, "call made");
   } catch (error) {
     console.log(error);
